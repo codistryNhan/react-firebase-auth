811c2c64aaabd04de7fa9d563e7b9574ab120405
Nhan <nhan4@msn.com>
2022-03-21 23:19:28 -0700

    removed sensitive info

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -18,17 +18,6 @@ const config = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-// const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
-// const config = {
-//   apiKey: "AIzaSyCbpKTn8DkXoHP5x-m9LIW400kIE3EP5Lw",
-//   authDomain: "fir-project-280ef.firebaseapp.com",
-//   projectId: "fir-project-280ef",
-//   storageBucket: "fir-project-280ef.appspot.com",
-//   messagingSenderId: "985783627655",
-//   appId: "1:985783627655:web:885badd5a1dbafbe8c2d22",
-//   measurementId: "G-BYHH2JE2SV",
-// };
-
 class Firebase {
   static count = 0;
 
