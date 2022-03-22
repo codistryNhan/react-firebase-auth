6a18e231f570158ed4a2788c69ea41b8e0039e91
Nhan <nhan4@msn.com>
2022-03-21 23:18:10 -0700

    renamed env file

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -9,16 +9,7 @@ import {
 } from "firebase/auth";
 import { getDatabase, ref, set as setFromDb } from "firebase/database";
 
-const prodConfig = {
-  apiKey: process.env.REACT_APP_PROD_API_KEY,
-  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_PROD_APP_ID,
-};
-
-const devConfig = {
+const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -28,15 +19,15 @@ const devConfig = {
 };
 
 // const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
-const config = {
-  apiKey: "AIzaSyCbpKTn8DkXoHP5x-m9LIW400kIE3EP5Lw",
-  authDomain: "fir-project-280ef.firebaseapp.com",
-  projectId: "fir-project-280ef",
-  storageBucket: "fir-project-280ef.appspot.com",
-  messagingSenderId: "985783627655",
-  appId: "1:985783627655:web:885badd5a1dbafbe8c2d22",
-  measurementId: "G-BYHH2JE2SV",
-};
+// const config = {
+//   apiKey: "AIzaSyCbpKTn8DkXoHP5x-m9LIW400kIE3EP5Lw",
+//   authDomain: "fir-project-280ef.firebaseapp.com",
+//   projectId: "fir-project-280ef",
+//   storageBucket: "fir-project-280ef.appspot.com",
+//   messagingSenderId: "985783627655",
+//   appId: "1:985783627655:web:885badd5a1dbafbe8c2d22",
+//   measurementId: "G-BYHH2JE2SV",
+// };
 
 class Firebase {
   static count = 0;
